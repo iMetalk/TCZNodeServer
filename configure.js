d7cd6d99065409eb33e7d56b57c32aef75b4972d
wangsuyan <wsyxyxs@126.com>
2016-12-01 22:25:54 +0800

    mysql

--- configure.js
+++ configure.js
@@ -1,6 +1,7 @@
 exports.configure = {
 	mysql: {
 	  host: 'localhost',
+	  port: '3306',
       user: 'root',
       password: 'qwe123',
       database: 'metalk'
--- mysql.js
+++ mysql.js
@@ -1,13 +1,17 @@
 
 var mysql = require('mysql');
+var configure = require('./configure.js');
 
 exports.configureMysql = function () {
 
+    var mysqlConfigure = configure.configure.mysql;
+
     var connection = mysql.createConnection({
-      host     : 'localhost',
-      user     : 'root',
-      password : 'qwe123',
-      database: 'metalk'
+      host     : mysqlConfigure.host,
+      user     : mysqlConfigure.user,
+      password : mysqlConfigure.password,
+      database : mysqlConfigure.password,
+      port     : mysqlConfigure.port
       // password : ';gb<V3a-NsD9'
     });
 
@@ -22,12 +26,21 @@ exports.configureMysql = function () {
     // Close connnetion
     // connection.end();
 
+    // SELECT
     var queryFriend = function (err, results, fields){
       console.log('query friend results:' + results[0].userId + ' ' + results[0].nickName);
       if (err) {
-          console.log('query friend error:' + err);
+          console.log('SELECT friend error:' + err);
       }
     };
     connection.query('SELECT *FROM friend', queryFriend);
+
+    // INSERT
+    var insertFrind = function (err){
+      if (err) {
+          console.log('INSERT friend error:' + err);
+      }
+    };
+    connection.query('INSERT INTO friend(userId, nickName) VALUES (?, ?)', ['23456', 'WSY'], insertFrind);
 
 }
